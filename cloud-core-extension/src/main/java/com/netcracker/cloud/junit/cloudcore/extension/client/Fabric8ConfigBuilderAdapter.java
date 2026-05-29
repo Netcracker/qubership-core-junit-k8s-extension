485cff32e128e621413293acdda6c267ca5e4940
Vladislav Larkin <larkin.qubership@gmail.com>
2026-05-29 16:07:31 +0400

    chore: archive repository, move source code to qubership-core-java-libs

--- cloud-core-extension/src/main/java/com/netcracker/cloud/junit/cloudcore/extension/client/Fabric8ConfigBuilderAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.netcracker.cloud.junit.cloudcore.extension.client;
-
-import io.fabric8.kubernetes.client.ConfigBuilder;
-
-public interface Fabric8ConfigBuilderAdapter {
-
-    ConfigBuilder adapt(ConfigBuilder configBuilder);
-}
--- cloud-core-extension/src/main/java/com/netcracker/cloud/junit/cloudcore/extension/client/Fabric8KubernetesClientBuilderAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.netcracker.cloud.junit.cloudcore.extension.client;
-
-import io.fabric8.kubernetes.client.KubernetesClientBuilder;
-
-public interface Fabric8KubernetesClientBuilderAdapter {
-
-    KubernetesClientBuilder adapt(KubernetesClientBuilder clientBuilder);
-}
